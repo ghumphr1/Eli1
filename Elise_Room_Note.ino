9765f680a2ff557c79c2d6d7ee1591688c8d7353
Geoff Norgrove-Humphries <ghumphr1@GeoffVaio.home>
2017-10-11 17:25:45 +0100

    1

--- Elise_Room_Note.ino
+++ Elise_Room_Note.ino
@@ -1,40 +1,5 @@
 /*
-  LiquidCrystal Library - Blink
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD and makes the
- cursor block blink.
-
- The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * 10K resistor:
-   * ends to +5V and ground
-   * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystalBlink
+Elise room notice
 
 */
 
@@ -65,18 +30,16 @@ void loop() {
 
     lcd.print("    Elise's Room    ");
     delay(500);
-    lcd.print("    *  OR ELSE *    ");  
+    lcd.print("    *  OR ELSE *    ");
     delay(500);
     lcd.print("                    ");
     lcd.print("                    ");
 
     lcd.print("    Elise's Room    ");
     delay(500);
-    lcd.print("    *  TROUBLE *    ");  
+    lcd.print("    *  TROUBLE *    ");
     delay(500);
     lcd.print("                    ");
     lcd.print("                    ");
-    
+
 }
-
-
--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /**
  * Irri2 Firmware
- * 
+ *
  * Copyright (C) 2014 Irri2Firmware [https://github.com/ghumphr1/Irri2]
  *
  * This program is free software: you can redistribute it and/or modify
@@ -45,47 +45,7 @@
  * You must increment this version number for every significant change such as,
  * but not limited to: ADD, DELETE RENAME OR REPURPOSE any directive/option.
  *
- * 
+ *
  */
 
 #define CONFIGURATION_H_VERSION 000001
-
-
-// debug output (enable/disable)
-#define DEBUG_MODE
-#ifdef DEBUG_MODE
-  #define DEBUG_PRINT(x)  Console.print(x)
-  #define DEBUG_PRINTLN(x)  Console.println(x)
-#else
-  #define DEBUG_PRINT(x)
-  #define DEBUG_PRINTLN(x)
-#endif
-
-// outputs, digital PINs connected to the relays
-#define OUT_1               2
-#define OUT_2               3
-#define OUT_3               4
-
-// leds, digital PINs that directly drive the panel leds
-#define LED_1               7
-#define LED_2               6
-#define LED_3               5
-
-// switches, analog PINs that read the 3-way panel switches
-#define SWITCH_1            0
-#define SWITCH_2            1
-#define SWITCH_3            2
-
-// rain sensor, digital PIN connected to the rain sensor
-// (basically a 2-way switch, normally closed)
-#define RAIN_SENSOR         8
-
-// constants for switch positions
-#define SWITCH_OFF          0
-#define SWITCH_AUTO         1
-#define SWITCH_ON           2
-
-// constants for rain sensor status
-#define NO_RAIN             0
-#define RAIN                1
-
